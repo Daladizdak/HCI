8c1eb3879a76db8423885a71245033f8427152d8
Daladizdak <a.h.shekarinamini@wlv.ac.uk>
2025-04-23 01:34:15 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,14 +1,14 @@
 const products = [
-  { id: 1, name: "T-Shirt", price: 15.00, category: "Clothing" },
-  { id: 2, name: "Jeans", price: 25.00, category: "Clothing" },
-  { id: 3, name: "Headphones", price: 60.00, category: "Electronics" },
-  { id: 4, name: "Bananas", price: 2.50, category: "Grocery" },
-  { id: 5, name: "Mouse", price: 30.00, category: "Electronics" },
-  { id: 6, name: "Bread", price: 1.50, category: "Grocery" },
-  { id: 7, name: "Hoodie", price: 35.00, category: "Clothing" },
-  { id: 8, name: "Laptop", price: 600.00, category: "Electronics" },
-  { id: 9, name: "Milk", price: 1.20, category: "Grocery" },
-  { id: 10, name: "Sneakers", price: 45.00, category: "Clothing" }
+  { id: 1, name: "T-Shirt", price: 15.00, category: "Clothing", image: "LV.png" },
+  { id: 2, name: "Jeans", price: 25.00, category: "Clothing", image: "gucci.png"  },
+  { id: 3, name: "Headphones", price: 60.00, category: "Electronics", image: "Supreme.png"  },
+  { id: 4, name: "Bananas", price: 2.50, category: "Fragrances", image: "Haltane.png"  },
+  { id: 5, name: "Mouse", price: 30.00, category: "Electronics", image: "5090.png"  },
+  { id: 6, name: "Bread", price: 1.50, category: "Fragrances", image: "Tweet.png"  },
+  { id: 7, name: "Hoodie", price: 35.00, category: "Electronics", image: "Asus.png"  },
+  { id: 8, name: "Laptop", price: 600.00, category: "Electronics", image: "Beats.png"  },
+  { id: 9, name: "Milk", price: 1.20, category: "Fragrances", image: "Megamare.png"  },
+  { id: 10, name: "Sneakers", price: 45.00, category: "Electronics", image: "ps5.png"  }
 ];
 
 function displayProducts() {
