1f7877e660a3a4a537c4d4116a46d418e393a664
Daladizdak <a.h.shekarinamini@wlv.ac.uk>
2025-04-23 01:45:07 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,14 +1,14 @@
 const products = [
-  { id: 1, name: "T-Shirt", price: 15.00, category: "Clothing", image: "LV.png" },
-  { id: 2, name: "Jeans", price: 25.00, category: "Clothing", image: "gucci.png"  },
-  { id: 3, name: "Headphones", price: 60.00, category: "Electronics", image: "Supreme.png"  },
-  { id: 4, name: "Bananas", price: 2.50, category: "Fragrances", image: "Haltane.png"  },
-  { id: 5, name: "Mouse", price: 30.00, category: "Electronics", image: "5090.png"  },
-  { id: 6, name: "Bread", price: 1.50, category: "Fragrances", image: "Tweet.png"  },
-  { id: 7, name: "Hoodie", price: 35.00, category: "Electronics", image: "Asus.png"  },
-  { id: 8, name: "Laptop", price: 600.00, category: "Electronics", image: "Beats.png"  },
-  { id: 9, name: "Milk", price: 1.20, category: "Fragrances", image: "Megamare.png"  },
-  { id: 10, name: "Sneakers", price: 45.00, category: "Electronics", image: "ps5.png"  }
+  { id: 1, name: "LV coat", price: 1000.00, category: "Clothing", image: "LV.png" },
+  { id: 2, name: "Gucci pullover", price: 500.00, category: "Clothing", image: "gucci.png"  },
+  { id: 3, name: "Supreme Hoodie", price: 60.00, category: "Electronics", image: "Supreme.png"  },
+  { id: 4, name: "PDM Haltane", price: 330.00, category: "Fragrances", image: "Haltane.png"  },
+  { id: 5, name: "Asus 5090 GPU", price: 4000.00, category: "Electronics", image: "5090.png"  },
+  { id: 6, name: "Green Irish Tweet", price: 280.00, category: "Fragrances", image: "Tweet.png"  },
+  { id: 7, name: "Asus monitor", price: 350.00, category: "Electronics", image: "Asus.png"  },
+  { id: 8, name: "Beats solo 2", price: 250.00, category: "Electronics", image: "Beats.png"  },
+  { id: 9, name: "Megamare", price: 150.00, category: "Fragrances", image: "Megamare.png"  },
+  { id: 10, name: "Sony PlayStation 5", price: 600.00, category: "Electronics", image: "ps5.png"  }
 ];
 
 function displayProducts() {
@@ -18,6 +18,7 @@ function displayProducts() {
     card.className = "card mb-3";
     card.innerHTML = `
       <div class="card-content">
+        <img src="${product.image}" alt="${product.name}" class="product-img">
         <h1>${product.name}</h1>
         <p>£${product.price.toFixed(2)}</p>
         <button class="btn btn-danger" data-id="${product.id}">Add to Basket</button>
